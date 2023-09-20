c6b3dcf84a10f68f43bae87d5701ee5dbc5961ca
darknebula05 <camms205@aol.com>
2023-09-20 14:43:09 -0400

    add sops input

--- flake.nix
+++ flake.nix
@@ -5,13 +5,15 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    sops-nix.url = "github:Mic92/sops-nix";
   };
 
-  outputs = {self, nixpkgs, home-manager, ...}@inputs: {
+  outputs = {self, nixpkgs, home-manager, sops-nix, ...}@inputs: {
     nixosConfigurations = {
       cam-desktop = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
+          sops-nix.nixosModules.sops
           ./hosts/cam-desktop
           home-manager.nixosModules.home-manager {
             home-manager.useGlobalPkgs = true;
@@ -23,6 +25,7 @@
       cam-laptop = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
+          sops-nix.nixosModules.sops
           ./hosts/cam-laptop
           home-manager.nixosModules.home-manager {
             home-manager.useGlobalPkgs = true;
--- hosts/cam-laptop/default.nix
+++ hosts/cam-laptop/default.nix
@@ -13,16 +13,16 @@
     "/nix".options = [ "compress=zstd" "noatime" ];
   };
 
-  boot.loader = {
-    efi = {
-      canTouchEfiVariables = true;
-      efiSysMountPoint = "/efi";
-    };
-    grub = {
-      efiSupport = true;
-      device = "nodev";
-    };
-  };
+  # boot.loader = {
+  #   efi = {
+  #     canTouchEfiVariables = true;
+  #     efiSysMountPoint = "/efi";
+  #   };
+  #   grub = {
+  #     efiSupport = true;
+  #     device = "nodev";
+  #   };
+  # };
 
   networking.hostName = "cam-laptop";
 
